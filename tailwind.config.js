8ac33a4af2aa2eb09b4291bf9a5425d9e2bf7a9c
lucaslmeireles <lluciomeireles@gmail.com>
2023-11-09 14:28:30 +0000

    init

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+const nativewind = require("nativewind/tailwind/native")
+
+module.exports = {
+  content: [
+    "./screens/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  // ...
+};
